0362682972bcdaeec9d46487aceaa9518e299ef3
Sir Nik <vikfed3717@gmail.com>
2024-10-18 22:09:49 +0300

    Task0

--- Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib/DataService.cs
+++ Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib/DataService.cs
@@ -1,13 +1,21 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
 namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib
 {
     public class DataService : ISprint2Task0V2
     {
-{
         public bool[] GetCompareOperations(int x, int y)
         {
-            throw new NotImplementedException();
+            bool[] result = new bool[6];
+
+            result[0] = x == y + 1; // False
+            result[1] = x * y > 10000; // True
+            result[2] = x / y <= 1; // False
+            result[3] = x - y >= 0; // True
+            result[4] = x != y * 2; // False
+            result[5] = x + y > 200; // True
+
+            return result;
         }
     }
-}
 }
--- Tyuiu.FedotkinNE.Sprint2.Task0.V2/Program.cs
+++ Tyuiu.FedotkinNE.Sprint2.Task0.V2/Program.cs
@@ -1,22 +1,15 @@
-﻿namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2
+﻿using Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib;
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
             int x = 123;
             int y = 123;
-
-            bool[] result = new bool[6];
-
-            result[0] = x == y + 1; // False
-            result[1] = x * y > 10000; // True
-            result[2] = x / y <= 1; // False
-            result[3] = x - y >= 0; // True
-            result[4] = x != y * 2; // False
-            result[5] = x + y > 200; // True
-
-            Console.WriteLine($"[{string.Join(", ", result)}]");
+            bool[] result = ds.GetCompareOperations(x, y);
         }
     }
 }
