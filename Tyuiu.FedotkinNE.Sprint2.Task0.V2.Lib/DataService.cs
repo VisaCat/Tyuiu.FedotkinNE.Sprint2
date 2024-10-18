5ad9d9b57fb9fddbaed68f843fcbd1685b35458a
Sir Nik <vikfed3717@gmail.com>
2024-10-18 22:06:15 +0300

    Add project files.

---
+++ Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib
+{
+    public class DataService : ISprint2Task0V2
+    {
+{
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
---
+++ Tyuiu.FedotkinNE.Sprint2.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
