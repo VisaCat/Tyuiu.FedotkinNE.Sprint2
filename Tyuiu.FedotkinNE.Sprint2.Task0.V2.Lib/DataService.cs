46b686cafa4452f67d6a88f2b0e45cbe0458cf93
Sir Nik <vikfed3717@gmail.com>
2024-10-18 22:32:56 +0300

    Task0

--- Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib/DataService.cs
+++ Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib/DataService.cs
@@ -8,11 +8,11 @@ namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib
         {
             bool[] result = new bool[6];
 
-            result[0] = x == y + 1; // False
+            result[0] = x == y-1; // False
             result[1] = x * y > 10000; // True
-            result[2] = x / y <= 1; // False
+            result[2] = x / y < 1; // False
             result[3] = x - y >= 0; // True
-            result[4] = x != y * 2; // False
+            result[4] = x != y; // False
             result[5] = x + y > 200; // True
 
             return result;
