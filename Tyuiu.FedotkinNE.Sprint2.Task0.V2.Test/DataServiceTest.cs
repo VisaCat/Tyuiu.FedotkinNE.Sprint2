c3725f21912b8f71729aa5ba82f6f2cb01363549
Sir Nik <vikfed3717@gmail.com>
2024-10-18 22:25:47 +0300

    Task0

--- Tyuiu.FedotkinNE.Sprint2.Task0.V2.Test/DataServiceTest.cs
+++ Tyuiu.FedotkinNE.Sprint2.Task0.V2.Test/DataServiceTest.cs
@@ -1,11 +1,25 @@
+using Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib;
 namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2.Test
 {
     [TestClass]
     public class DataServiceTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        
+            [TestMethod]
+            public void GetCompareOperationsTest()
+            {
+                // Arrange
+                DataService ds = new DataService();
+                int x = 123;
+                int y = 123;
+                bool[] expectedResult = new bool[] { false, true, false, true, false, true };
+
+                // Act
+                bool[] actualResult = ds.GetCompareOperations(x, y);
+
+                // Assert
+                Assert.AreEqual(expectedResult, actualResult);
+
+            }
     }
 }
