5becf31af6193b33e6b20bd4f07b3edb10fd9723
Sir Nik <vikfed3717@gmail.com>
2024-12-04 23:33:50 +0300

    Task1

---
+++ Tyuiu.FedotkinNE.Sprint2.Task1.V6.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿namespace Tyuiu.FedotkinNE.Sprint2.Task1.V6.Lib
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+{
+    public class DataService : ISprint2Task1V6
+    {
+{
+    public bool[] GetLogicOperations(int a, int b, int c, int d)
+    {
+        bool[] result = new bool[] {
+            !(a > b),
+            !(c >= d) && (a == b),
+            (a > b) || (c < d),
+            !(d > b) && (c != a),
+            (a > b) || (c < d),
+            !(a == b) && (c > d)
+        };
+        return result;
+    }
+}
+}
+}
---
+++ Tyuiu.FedotkinNE.Sprint2.Task1.V6/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.FedotkinNE.Sprint2.Task1.V6.Lib;
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 915;
+            int b = 169;
+            int c = 174;
+            int d = 133;
+            bool[] result = ds.GetLogicOperations(a, b, c, d);
+
+            // Print the results
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
