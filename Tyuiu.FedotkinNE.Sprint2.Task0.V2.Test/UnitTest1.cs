bda94b185c10894143306e8317dfb53b575f0f15
Sir Nik <vikfed3717@gmail.com>
2024-12-04 23:17:35 +0300

    Add project files.

---
+++ Tyuiu.FedotkinNE.Sprint2.Task0.V2.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.FedotkinNE.Sprint2.Task0.V2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.FedotkinNE.Sprint2.Task0.V2.Lib;
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task0.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 123;
+            int y = 123;
+            bool[] result = ds.GetCompareOperations(x, y);
+
+            // Print the results
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
