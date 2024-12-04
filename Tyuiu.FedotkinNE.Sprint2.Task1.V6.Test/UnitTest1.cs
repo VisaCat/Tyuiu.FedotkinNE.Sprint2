2cef04e882125fecf6b82dd3628fb13709a3bbeb
Sir Nik <vikfed3717@gmail.com>
2024-12-04 23:34:39 +0300

    Task1

---
+++ Tyuiu.FedotkinNE.Sprint2.Task1.V6.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.FedotkinNE.Sprint2.Task1.V6.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
