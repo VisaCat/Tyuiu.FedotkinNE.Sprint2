89d3b88d988bad870d7de87776758025dbee49de
Sir Nik <vikfed3717@gmail.com>
2024-12-05 16:40:52 +0300

    Task5

--- Tyuiu.FedotkinNE.Sprint2.Task5.V2.Lib/DataService.cs
+++ Tyuiu.FedotkinNE.Sprint2.Task5.V2.Lib/DataService.cs
@@ -6,7 +6,34 @@ namespace Tyuiu.FedotkinNE.Sprint2.Task5.V2.Lib
     {
         public string FindMonthSeason(int value)
         {
-            throw new NotImplementedException();
+            string time;
+            switch (value)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    time = "Зима";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    time = "Весна";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    time = "Лето";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    time = "Осень";
+                    break;
+                default:
+                    time = "Некорректный номер месяца";
+                    break;
+            }
+            return time;
         }
     }
 }
--- Tyuiu.FedotkinNE.Sprint2.Task5.V2/Program.cs
+++ Tyuiu.FedotkinNE.Sprint2.Task5.V2/Program.cs
@@ -8,36 +8,14 @@ namespace Tyuiu.FedotkinNE.Sprint2.Task5.V2
             Console.WriteLine("Введите номер месяца (1-12):");
             int month = Convert.ToInt32(Console.ReadLine());
 
-            string season;
 
-            switch (month)
-            {
-                case 12:
-                case 1:
-                case 2:
-                    season = "Зима";
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                    season = "Весна";
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    season = "Лето";
-                    break;
-                case 9:
-                case 10:
-                case 11:
-                    season = "Осень";
-                    break;
-                default:
-                    season = "Некорректный номер месяца";
-                    break;
-            }
+            DataService ds = new DataService();
+            string time = ds.FindMonthSeason(month);
 
-            Console.WriteLine($"Месяц {month} относится к {season}");
+
+
+
+            Console.WriteLine($"Месяц {month} относится к {time}");
         }
     }
 }
