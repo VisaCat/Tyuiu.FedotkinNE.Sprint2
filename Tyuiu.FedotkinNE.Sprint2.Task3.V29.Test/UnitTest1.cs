2041998397e8a2c2e5aebc7cb822361200112091
Sir Nik <vikfed3717@gmail.com>
2024-12-05 16:02:01 +0300

    Task3

---
+++ Tyuiu.FedotkinNE.Sprint2.Task3.V29.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.FedotkinNE.Sprint2.Task3.V29.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.FedotkinNE.Sprint2.Task3.V29/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.FedotkinNE.Sprint2.Task3.V29.Lib;
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task3.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            DataService ds = new DataService();
+            double y = ds.Calculate(x);
+            Console.WriteLine($"Y = {Math.Round(y, 3)}");
+        }
+    }
+}
