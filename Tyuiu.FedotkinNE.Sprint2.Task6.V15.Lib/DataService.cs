2645d014818b5a051f5133df4be668ac82974ab9
Sir Nik <vikfed3717@gmail.com>
2024-12-05 16:56:15 +0300

    Task6

---
+++ Tyuiu.FedotkinNE.Sprint2.Task6.V15.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task6.V15.Lib
+{
+    public class DataService : ISprint2Task6V15
+    {
+        public string FindDayName(int k)
+        {
+            string dayOfWeek;
+            switch ((k - 1) % 7)
+            {
+                
+                case 0:
+                dayOfWeek = "Понедельник";
+                break;
+            case 1:
+                dayOfWeek = "Вторник";
+                break;
+            case 2:
+                dayOfWeek = "Среда";
+                break;
+            case 3:
+                dayOfWeek = "Четверг";
+                break;
+            case 4:
+                dayOfWeek = "Пятница";
+                break;
+            case 5:
+                dayOfWeek = "Суббота";
+                break;
+            case 6:
+                dayOfWeek = "Воскресенье";
+                break;
+            default:
+                dayOfWeek = "Ошибка"; // Этот случай не должен происходить
+                break;
+            }
+            return dayOfWeek;
+        }
+    }
+}
---
+++ Tyuiu.FedotkinNE.Sprint2.Task6.V15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tyuiu.FedotkinNE.Sprint2.Task6.V15.Lib;
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task6.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Write("Введите номер дня (k): ");
+            int k = int.Parse(Console.ReadLine());
+
+            // Убедимся, что k в пределах от 1 до 365
+            if (k < 1 || k > 365)
+            {
+                Console.WriteLine("Пожалуйста, введите число от 1 до 365.");
+                return;
+            }
+            string Res = ds.FindDayName(k);
+            Console.WriteLine($"День недели для {k}-го дня: {Res}");
+        }
+    }
+}
