03f145812e31136ea3bb2fbce772e6a104da5fe4
Sir Nik <vikfed3717@gmail.com>
2024-12-05 21:28:26 +0300

    Task7

---
+++ Tyuiu.FedotkinNE.Sprint2.Task7.V15.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task7.V15.Lib
+{
+    public class DataService : ISprint2Task7V15
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            double radius = 2;
+            double radius2 = 1;
+
+            double distance = Math.Sqrt(x * x + y * y);
+            if ((distance < radius) && (distance > radius2))
+            {
+                return res = true;
+            }
+            else if ((distance == radius) || (distance == radius2))
+            {
+                return res = true;
+            }
+            else
+            {
+                return res = false;
+            }
+        }
+    }
+}
---
+++ Tyuiu.FedotkinNE.Sprint2.Task7.V15/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.FedotkinNE.Sprint2.Task7.V15.Lib;
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task7.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            // Запрос координат точки у пользователя
+            Console.WriteLine("Введите координаты точки (X, Y):");
+
+            Console.Write("X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            bool res;
+            Console.WriteLine(res = ds.CheckDotInShadedArea(x, y));
+        }
+    }
+}
