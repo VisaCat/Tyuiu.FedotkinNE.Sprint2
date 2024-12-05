03bb78be1304ac49db0837c26caacb9173797ae6
Sir Nik <vikfed3717@gmail.com>
2024-12-05 16:27:49 +0300

    Task4

--- Tyuiu.FedotkinNE.Sprint2.Task4.V20.Lib/DataService.cs
+++ Tyuiu.FedotkinNE.Sprint2.Task4.V20.Lib/DataService.cs
@@ -7,8 +7,8 @@ namespace Tyuiu.FedotkinNE.Sprint2.Task4.V20.Lib
         public double Calculate(double x, double y)
         {
             double z = (x + 10 > y * 2)
-            ? x * Math.Pow((y + (1 / x) - 1), x)
-            : Math.Pow(x, 2) - 2 * (1 - (1 / y));
+            ? x * Math.Pow(((y + 1) / (x - 1)), x)
+            : Math.Pow(x, 2) * (1 / y);
             return Math.Round(z, 3);
         }
     }
