650d4a3508ab4dfbcca6d408b2e85cb488588406
Sir Nik <vikfed3717@gmail.com>
2024-12-05 16:41:48 +0300

    Task5

--- Tyuiu.FedotkinNE.Sprint2.Task5.V2.Lib/DataService.cs
+++ Tyuiu.FedotkinNE.Sprint2.Task5.V2.Lib/DataService.cs
@@ -12,22 +12,22 @@ namespace Tyuiu.FedotkinNE.Sprint2.Task5.V2.Lib
                 case 12:
                 case 1:
                 case 2:
-                    time = "Зима";
+                    time = "зима";
                     break;
                 case 3:
                 case 4:
                 case 5:
-                    time = "Весна";
+                    time = "весна";
                     break;
                 case 6:
                 case 7:
                 case 8:
-                    time = "Лето";
+                    time = "лето";
                     break;
                 case 9:
                 case 10:
                 case 11:
-                    time = "Осень";
+                    time = "осень";
                     break;
                 default:
                     time = "Некорректный номер месяца";
