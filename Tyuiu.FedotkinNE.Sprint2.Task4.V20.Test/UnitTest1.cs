f4a50694a1f1e491cff383d0d0b5148d1e069145
Sir Nik <vikfed3717@gmail.com>
2024-12-05 16:25:10 +0300

    Task4

---
+++ Tyuiu.FedotkinNE.Sprint2.Task4.V20.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.FedotkinNE.Sprint2.Task4.V20.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.FedotkinNE.Sprint2.Task4.V20/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.FedotkinNE.Sprint2.Task4.V20.Lib;
+
+namespace Tyuiu.FedotkinNE.Sprint2.Task4.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("Введите значение x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x, y);
+            Console.WriteLine(res);
+
+        }
+    }
+}
